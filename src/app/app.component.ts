5322a6d934c58c084da5ce8090491618bcfa3251
JosehGaks <josehwilddog@gmail.com>
2020-01-13 16:07:01 +0300

    add quote import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { QuoteComponent } from './quote/quote.component';
+QuoteComponent
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuotDetailsComponent } from './quot-details/quot-details.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuotDetailsComponent,
+    QuoteFormComponent,
+    DateCountPipe,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
