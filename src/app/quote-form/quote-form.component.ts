73b481260438cd90da1c83346f5cb270ba275a39
JosehGaks <josehwilddog@gmail.com>
2020-01-13 16:13:29 +0300

    create an addQuote object

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -12,8 +12,7 @@ export class QuoteFormComponent implements OnInit {
 
   @Output() addQuote = new EventEmitter<Quote>();
   
-  
- submitQuote(){
+  submitQuote(){
     
     this.addQuote.emit(this.newQuote);
   }
