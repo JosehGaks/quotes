c3ced427beb6173ed62cfd15f91d581bec5d1946
JosehGaks <josehwilddog@gmail.com>
2020-01-13 16:35:00 +0300

    edit the appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { QuoteComponent } from './quote/quote.component';
-QuoteComponent
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
